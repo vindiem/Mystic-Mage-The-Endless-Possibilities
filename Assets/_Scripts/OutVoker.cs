55bf0d8d34fa76925272608e759beedfc9f041fb
Vitya <vitiakyselov1@gmail.com>
2023-01-29 07:07:21 +0200

    sum
    mm

--- Assets/_Scripts/OutVoker.cs
+++ Assets/_Scripts/OutVoker.cs
@@ -7,41 +7,88 @@ using UnityEngine;
 public class OutVoker : MonoBehaviour
 {
     public LayerMask CastLayer;
+    private Vector3 currentMousePosition = new Vector3();
+
     private Vector3 targetPosition = new Vector3();
-    
-    private void Start()
-    {
-        
-    }
+    private Quaternion lookRotation;
+
+    // character variables
+    private int movementSpeed = 6;
+    private int rotationSpeed = 10;
 
     private void Update()
     {
+        #region Get current mouse position
+
         Ray mouseWorldPosition = Camera.main.ScreenPointToRay(Input.mousePosition);
-        
-        #region get mouse position
-        
+
         if (Physics.Raycast(mouseWorldPosition, out RaycastHit raycastHit, CastLayer))
         {
-            // transform.position = raycastHit.point;
-            targetPosition = raycastHit.point;
+            currentMousePosition = raycastHit.point;
+        }
+        currentMousePosition.y = 0;
+
+        #endregion
+
+        #region Set target position
+
+        // ... & set target position (if right mouse button has pressed)
+        if (Input.GetMouseButtonDown(1))
+        {
+            // Set look rotation
+            lookRotation = Quaternion.LookRotation(new Vector3(currentMousePosition.x - transform.position.x, 
+                0, currentMousePosition.z - transform.position.z));
+
+            targetPosition = currentMousePosition;
         }
 
         #endregion
-        
-        // movement
-        
-        if (Input.GetMouseButton(1))
+
+        RotateToMouse();
+
+        #region Movement relatively target position
+
+        Vector3 transformPositionXZ = new Vector3(transform.position.x, 0f, transform.position.z);
+        Vector3 targetPositionXZ = new Vector3(targetPosition.x, 0f, targetPosition.z);
+
+        if (transformPositionXZ != targetPositionXZ)
         {
-            //transform.position = Vector3.MoveForwards();
-            transform.position = mousePosition.position;
+            // Rotating to target position
+            transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
+
+            // Moving to target position
+            targetPositionXZ.y = transform.position.y;
+            transform.position = Vector3.MoveTowards(transform.position, targetPositionXZ, movementSpeed * Time.deltaTime);
         }
-        
-        // look to the mouse position
-        else if (Input.GetKey(KeyCode.F) == true)
-        {
-            transform.LookAt(new Vector3(targetPosition.x, transform.position.y, targetPosition.z));
-        }
+
+        #endregion
 
     }
 
+    private void RotateToMouse()
+    {
+        // look to mouse position
+        if (Input.GetKey(KeyCode.F) == true)
+        {
+            transform.LookAt(new Vector3(currentMousePosition.x, transform.position.y, currentMousePosition.z));
+            // Look At transform main character with his own axis (y) and target position (x, z)
+            // In 2D (x, z) = (x, y)
+        }
+    }
+
 }
+
+public class Skills : OutVoker
+{
+    private void rotateToMouse()
+    {
+        // look to mouse position
+        if (Input.GetKey(KeyCode.F) == true)
+        {
+            //transform.LookAt(new Vector3(currentMousePosition.x, transform.position.y, currentMousePosition.z));
+            // Look At transform main character with his own axis (y) and target position (x, z)
+            // In 2D (x, z) = (x, y)
+        }
+    }
+}
+
