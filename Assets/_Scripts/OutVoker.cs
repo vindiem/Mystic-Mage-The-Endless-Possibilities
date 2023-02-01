002b1ab6bc4c2dd79025c0c1aff1a72344b99959
Vitya <vitiakyselov1@gmail.com>
2023-02-01 12:46:40 +0200

    bugsssssss fix
    fucking bug with animation fixed. IDK what I did that. DON'T TOUCH ANIMATIONS. Adds some skills: meteors, blivk, [G][A]

--- Assets/_Scripts/OutVoker.cs
+++ Assets/_Scripts/OutVoker.cs
@@ -7,18 +7,58 @@ using UnityEngine;
 public class OutVoker : MonoBehaviour
 {
     public LayerMask CastLayer;
-    private Vector3 currentMousePosition = new Vector3();
+    //private Rigidbody rigidbody;
 
+    // Vectors
+    private Vector3 currentMousePosition = new Vector3();
     private Vector3 targetPosition = new Vector3();
     private Quaternion lookRotation;
 
-    // character variables
+    [Header("Charavter variabels")]
     private int movementSpeed = 6;
     private int rotationSpeed = 10;
     private Animator animator;
+    public int force = 0;
+
+    [Header("Items")]
+    public GameObject mouseEffect;
+    public GameObject meteor;
+
+    // Skills k/d's
+    private List<float> KDs = new List<float>();
+
+    // TAB - 0
+
+    // Q - 1++
+    // W - 2
+    // E - 3
+    // R - 4
+    // T - 5
+    // Y - 6
+
+    // A - 7++
+    // S - 8
+    // D - 9
+    // F - 10--
+    // G - 11++
+    // H - 12
+
+    // Z - 13
+    // X - 14
+    // C - 15
+    // V - 16
+    // B - 17
+    // N - 18
 
     private void Start()
     {
+        // Make KDs array full
+        for (int i = 0; i <= 18; i++)
+        {
+            KDs.Add(0);
+        }
+
+        //rigidbody = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
     }
 
@@ -46,11 +86,19 @@ public class OutVoker : MonoBehaviour
                 0, currentMousePosition.z - transform.position.z));
 
             targetPosition = currentMousePosition;
+
+            // Instantiate mouse effect
+            GameObject effect = Instantiate(mouseEffect, new Vector3(currentMousePosition.x, currentMousePosition.y + 0.51f, 
+                currentMousePosition.z), Quaternion.LookRotation(Vector3.up));
         }
 
         #endregion
 
         RotateToMouse();
+
+        Dash();
+        Meteor();
+        Arrows();
 
         #region Movement relatively target position
 
@@ -79,11 +127,45 @@ public class OutVoker : MonoBehaviour
 
         #endregion
 
+        #region KD
+        
+        for (int i = 0; i < KDs.Count; i++)
+        {
+            if (KDs[i] > 0)
+            {
+                KDs[i] -= Time.deltaTime;
+            }
+        }
+        
+        #endregion
+
     }
 
-    // Q, W, E, R, T, Y
-    // A, S, D, [F], G, H
+    // [Q], W, E, R, T, Y
+    // [A], S, D, {F}, [G], H
     // Z, X, C, V, B, N
+    // TAB
+
+    // [Q]
+    private void Arrows()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && KDs[1] <= 0)
+        {
+
+        }
+    }
+
+    // [A]
+    private void Dash()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && KDs[7] <= 0)
+        {
+            transform.Translate(Vector3.forward * force);
+            targetPosition = transform.position;
+
+            KDs[7] = 10;
+        }
+    }
 
     // [F]
     private void RotateToMouse()
@@ -95,14 +177,23 @@ public class OutVoker : MonoBehaviour
             // In 2D (x, z) = (x, y)
             transform.LookAt(new Vector3(currentMousePosition.x, transform.position.y, currentMousePosition.z));
 
-            // Set target position to player position to stop caracter
+            // Set target position to player position to stop character
             targetPosition = transform.position;
 
         }
     }
 
-    // [S]
+    // [G]
+    private void Meteor()
+    {
+        if (Input.GetKeyDown(KeyCode.G) && KDs[11] <= 0)
+        {
+            GameObject m = Instantiate(meteor, new Vector3(currentMousePosition.x, 
+                transform.position.y + 9f, currentMousePosition.z), Quaternion.identity);
 
+            KDs[11] = 20;
+        }
+    }
 
 }
 
