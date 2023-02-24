6b776af2b264b7c0e12811e7176068075d56bbe1
Vitya <vitiakyselov1@gmail.com>
2023-02-24 23:49:17 +0200

    Settings
    - Sound settings added
    - Quality settings added
    - Resolution settings added

--- Assets/_Scripts/Menu/MenuSettings.cs
+++ Assets/_Scripts/Menu/MenuSettings.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,8 +10,27 @@ public class MenuSettings : MonoBehaviour
     public Image selectIndicatorJoystick;
     private string movementType;
 
+    public Slider volumeSlider;
+    public Slider qualitySlider;
+
+    public Dropdown resolutionsDropdown;
+    private Resolution[] resolutions;
+
     private void Start()
     {
+        volumeSlider.value = PlayerPrefs.GetFloat("Volume");
+        qualitySlider.value = PlayerPrefs.GetInt("Quality");
+
+        Resolution[] resolutions = Screen.resolutions;
+        resolutions = resolutions.Distinct().ToArray();
+        string[] strRes = new string[resolutions.Length];
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            strRes[i] = resolutions[i].ToString();
+        }
+        resolutionsDropdown.ClearOptions();
+        resolutionsDropdown.AddOptions(strRes.ToList());
+
     }
 
     private void Update()
@@ -48,6 +68,25 @@ public class MenuSettings : MonoBehaviour
             selectIndicatorButton.gameObject.SetActive(false);
             selectIndicatorJoystick.gameObject.SetActive(true);
         }
+    }
+
+    public void VolumeSet()
+    {
+        float volumeValue = volumeSlider.value;
+        PlayerPrefs.SetFloat("Volume", volumeValue);
+    }
+
+    public void QualitySet()
+    {
+        int qualityValue = (int)qualitySlider.value;
+        PlayerPrefs.SetInt("Quality", qualityValue);
+        QualitySettings.SetQualityLevel(qualityValue, true);
+    }
+
+    public void SetResolution()
+    {
+        Screen.SetResolution(resolutions[resolutionsDropdown.value].width, 
+            resolutions[resolutionsDropdown.value].height, true);
     }
 
 }
